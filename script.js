ed3c414019fc2e37b38c4657f3c4d700c80f4680
Molankar Bhagyasri <86193546+MolankarBhagyasri@users.noreply.github.com>
2021-08-10 11:09:07 +0530

    calcy

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-function publishResult(){
-    const Cprogarmming=document.getElementById("C").value;
-    const java=document.getElementById("java").value;
-    const HTML=document.getElementById("Web").value;
-    const css=document.getElementById("css").value;
-    const Nodejs=document.getElementById("Node").value;
+function publishResult()
+{
+    const Cprogarmming  =   document.getElementById("C").value;
+    const java          =   document.getElementById("java").value;
+    const HTML          =   document.getElementById("Web").value;
+    const css           =   document.getElementById("css").value;
+    const Nodejs        =   document.getElementById("Node").value;
 
-    let total= parseFloat(Cprogarmming) + parseFloat(java) + parseFloat(HTML) + parseFloat(css) + parseFloat(Nodejs);
-    document.getElementById("sum").innerHTML="The Sum Is:" + total;
-    let percentage=(total*100)/500;
-    document.getElementById("percentage").innerHTML="The Percentage Is:" + percentage;
+        let total =  parseFloat(Cprogarmming) +
+        parseFloat(java) + parseFloat(HTML)   +
+        parseFloat(css) + parseFloat(Nodejs);
+    
+    document.getElementById("sum").innerHTML="The Sum Is :" + total;
+    let percentage = (total*100)/500;
+    document.getElementById("percentage").innerHTML="The Percentage Is :" + percentage;
 
-    if (percentage>=90) {
+    if (percentage >= 90)
+    {
        document.getElementById("grade").innerHTML="Youre Passed In A+ Grade";
     }
-    else if (percentage>=80) {
+    else if (percentage >= 80)
+    {
         document.getElementById("grade").innerHTML="Youre  Passed In A Grade";
     }
-    else if (percentage>=70) {
+    else if (percentage >= 70)
+    {
         document.getElementById("grade").innerHTML="Youre  Passed In B+ Grade";
     }
-    else if (percentage>=60) {
+    else if (percentage >= 60)
+    {
         document.getElementById("grade").innerHTML="Youre Passed In B Grade";
     }
-    else if (percentage>=50) {
+    else if (percentage >= 50)
+    {
         document.getElementById("grade").innerHTML="Youre  Passed In C+ Grade";
     }
-    else if (percentage>=40) {
+    else if (percentage >= 40)
+    {
         document.getElementById("grade").innerHTML="Youre  Passed In C Grade";
     }
     else if (percentage>=30) {
